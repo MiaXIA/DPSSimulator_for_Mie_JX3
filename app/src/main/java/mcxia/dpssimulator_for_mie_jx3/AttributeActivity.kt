a3ae018bae1002c4a45b55db81ec5fc01ab403cb
McX <summer.mc.xia@gmail.com>
2017-11-11 20:35:52 -0500

    test commit

---
+++ app/src/main/java/mcxia/dpssimulator_for_mie_jx3/AttributeActivity.kt
@@ -0,0 +1,12 @@
+package mcxia.dpssimulator_for_mie_jx3
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AttributeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_attribute)
+    }
+}
